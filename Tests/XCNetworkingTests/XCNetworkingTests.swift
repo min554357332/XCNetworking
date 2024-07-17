4caf918a2091cf3a417b5460315da2c0cf6b2d1b
大江山岚 <554357332@qq.com>
2024-07-17 14:30:28 +0800

    no message

--- Tests/XCNetworkingTests/XCNetworkingTests.swift
+++ Tests/XCNetworkingTests/XCNetworkingTests.swift
@@ -15,47 +15,10 @@ final class XCNetworkingTests: XCTestCase {
             print(failure)
         }
     }
-    // (Result<Data,NWError>) -> Void
+    
     func streamHandler(_ result: Data) {
         let string = String(data: result, encoding: .utf8)
         print(string)
-    }
-    var expectation: XCTestExpectation!
-    func testAaa() {
-        /*
-         curl --location 'http://18.220.53.146:10087/api/v1/search/stream' \
-         -X POST \
-         -H 'Content-Type: application/json' \
-         -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo1NTQzLCJyZWdpc3Rlcl90eXBlIjoicGhvbmUiLCJhcHBfbmFtZSI6IkNoaXRDaGF0X2lPUyIsInRva2VuX2lkIjoiYjUyNWIxMzItZmM2My00YWMwLTgxN2QtMjdmZjczNDZlYzliIiwiaXNzIjoiZGV2Lndpc2Vob29kLmFpIiwiYXVkIjpbIiJdLCJleHAiOjE3NDI2OTUzNzgsIm5iZiI6MTcxMTU5MTM3OCwiaWF0IjoxNzExNTkxMzc4fQ.5ipqxQhCJKeYCZ2ezNZlJrvb7Fc_R1bcZYuIzmEHZaU' \
-         -d '{"query": "今天几号"}'
-         */
-        let url = "http://18.220.53.146:10087/api/v1/search/stream"
-        self.expectation = expectation(description: "Login ···")
-        let model = RequestModel(query: "今天几号")
-        AF.streamRequest(
-            url,
-            method: .post,
-            parameters: model,
-            encoder: JSONParameterEncoder.default,
-            headers: HTTPHeaders(["Authorization":testToken]),
-            automaticallyCancelOnStreamError: true,
-            interceptor: nil,
-            requestModifier: nil
-        )
-        .responseStream { result in
-            switch result.event {
-            case let .stream(result):
-                switch result {
-                case let .success(data):
-                    let string = String(data: data, encoding: .utf8)
-                    print(string)
-                }
-            case let .complete(completion):
-                print(completion)
-                self.expectation.fulfill()
-            }
-        }
-        self.wait(for: [self.expectation], timeout: 300)
     }
 }
 
@@ -74,7 +37,7 @@ class BaseRequest<T: Json>: NWRequest<T> {
     }
     
     override func host() -> String {
-        return "18.220.53.146:10087"
+        return "localhost:8888"
     }
     
     override func method() -> NWMethod {
@@ -88,7 +51,7 @@ class BaseRequest<T: Json>: NWRequest<T> {
 
 class XCTee<T: Json>: BaseRequest<T> {
     override func path() -> String {
-        return "/api/v1/search/stream"
+        return "/api/v1/completions"
     }
     
     override func timeout() -> TimeInterval {
@@ -117,5 +80,4 @@ class BaseRequestInterceptor: NWInterceptor {
 }
 
 let testToken = """
-Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo1NTQzLCJyZWdpc3Rlcl90eXBlIjoicGhvbmUiLCJhcHBfbmFtZSI6IkNoaXRDaGF0X2lPUyIsInRva2VuX2lkIjoiYjUyNWIxMzItZmM2My00YWMwLTgxN2QtMjdmZjczNDZlYzliIiwiaXNzIjoiZGV2Lndpc2Vob29kLmFpIiwiYXVkIjpbIiJdLCJleHAiOjE3NDI2OTUzNzgsIm5iZiI6MTcxMTU5MTM3OCwiaWF0IjoxNzExNTkxMzc4fQ.5ipqxQhCJKeYCZ2ezNZlJrvb7Fc_R1bcZYuIzmEHZaU
 """
